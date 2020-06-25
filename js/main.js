ed0f3d0d512044d02de9ea7f8cabc7a4fea17b65
Aysel Mammadova <ayselmemmedova9317@gmail.com>
2020-06-25 13:01:22 +0400

    New changes

--- js/main.js
+++ js/main.js
@@ -48,46 +48,29 @@ $(document).ready(function () {
         if (about - ($(window).scrollTop()+$(window).height()) < 0) {
             $('#about .articleSec').addClass('fadeInLeft');
             $('#about .hexagon-img').addClass('zoomIn');
-        } else {
-            $('#about .articleSec').removeClass('fadeInLeft');
-            $('#about .hexagon-img').removeClass('zoomIn');
-        };
+        } 
 
         if (services - ($(window).scrollTop()+$(window).height()) < 0) {
             $('#services .box-wrap').addClass('fadeInRight');
-        } else {
-            $('#services .box-wrap').removeClass('fadeInRight');
-        };
+        } 
 
         if (news - ($(window).scrollTop()+$(window).height()) < 0) {
             $('#news .news-desc').addClass('fadeInLeft');
             $('#news .box-wrap').addClass('zoomIn');
-        } else {
-            $('#news .news-desc').removeClass('fadeInLeft');
-            $('#news .box-wrap').removeClass('zoomIn');
-        };
+        } 
 
         if (footerTop - ($(window).scrollTop()+$(window).height()) < 0) {
             $('footer .section-top h4').addClass('fadeInRight');
             $('footer .section-top .socialM').addClass('fadeInRight');
             $('footer .section-top .socialName').addClass('fadeInRight');
-        } else {
-            $('footer .section-top h4').removeClass('fadeInRight');
-            $('footer .section-top .socialM').removeClass('fadeInRight');
-            $('footer .section-top .socialName').removeClass('fadeInRight');
-        }
+        } 
 
         if (footerBottom - ($(window).scrollTop()+$(window).height()) < 0) {
             $('footer .section-bottom .navbar-brand').addClass('zoomIn');
             $('footer .section-bottom .h6').addClass('fadeIn');
             $('footer .section-bottom h6').addClass('fadeIn');
             $('footer .section-bottom li').addClass('fadeIn');
-        } else {
-            $('footer .section-bottom .navbar-brand').removeClass('zoomIn');
-            $('footer .section-bottom .h6').removeClass('fadeIn');
-            $('footer .section-bottom h6').removeClass('fadeIn');
-            $('footer .section-bottom li').removeClass('fadeIn');
-        }
+        } 
     }); 
 
 
