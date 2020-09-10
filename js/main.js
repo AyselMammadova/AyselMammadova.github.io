7e1ca502b90de8da3889f0e4c3eabe8965826686
Aysel Mammadova <ayselmemmedova9317@gmail.com>
2020-09-10 19:09:15 +0400

    az kalsi company

--- js/main.js
+++ js/main.js
@@ -1,126 +1,59 @@
 $(document).ready(function () {
 
-    $('.mainCarousel.first-Main').owlCarousel({
-        loop: true,
-        lazyLoad:true,
-        autoplay: true,
-        dots: false,
-        nav: true,
-        navSpeed: 2000,
-        animateOut: 'slideOutLeft',
-        navText: ["<i class='fas fa-angle-left'></i>","<i class='fas fa-angle-right'></i>"],
-        items: 1
+    // scroll down
+    $(".scroll-down").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about-company").offset().top
+        }, 2000);
     });
 
-    $('.mainCarousel.second-Main').owlCarousel({
-        loop: true,
-        lazyLoad:true,
-        autoplay: true,
-        dots: false,
-        nav: true,
-        navSpeed: 500,
-        navText: ["<i class='fas fa-angle-left'></i>","<i class='fas fa-angle-right'></i>"],
-        items: 1
+    // window scroll
+
+    $(window).scroll(function(){
+        var feature = $('#features'),
+            header = $('header'),
+            scroll = $(window).scrollTop();
+        var featureHeight = $('#features').height();
+        var headHeight = $('header').height();
+      
+        if (scroll >= featureHeight) {
+            feature.slideUp('500');
+            feature.css('display', 'none');
+        }
+        else {
+            feature.css('display', 'flex');
+        }
+      
+        if (feature.is(':hidden')) header.addClass('sticky')
+        else header.removeClass('sticky');
     });
 
-    $('.news-carousel').owlCarousel({
+
+    // product carousel
+
+    $('.products-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        dots: false,
+        nav: true,
+        items: 5, 
         margin: 10,
-        loop: true,
-        lazyLoad:true,
-        autoplay: true,
-        dots: false,
-        nav: true,
-        navSpeed: 500,
-        navContainer: '.navCustom',
-        navText: ["<i class='fas fa-angle-left'></i>","<i class='fas fa-angle-right'></i>"],
-        items: 3
-    });
-
-    // when hover items
-    
-    $('.owl-carousel .item').mouseover(function(){
-    $(this).trigger('stop.owl.autoplay');
-    });
-    
-    $('.owl-carousel .item').mouseleave(function(){
-        $(this).trigger('play.owl.autoplay',[5000]);
-    });
-    
-    //when click navs
-    
-    $('section .owl-nav > button').on('click', function () {
-        $('.owl-carousel').trigger('stop.owl.autoplay');
-    });
-    
-    $('section .owl-nav > button').mouseover(function(){
-        $('.owl-carousel').trigger('play.owl.autoplay',[5000]);
-    });
-    
-    $('section .owl-nav > button').mouseleave(function(){
-        $('.owl-carousel').trigger('play.owl.autoplay',[5000]);
-    });
-
-
-    $('.search-icon').click(function(e) {
-        e.stopPropagation(); 
-        $(this).find('i').toggleClass('fa-search fa-times');
-        $('.search-line').slideToggle("500");
-    });
-
-    $('.search-line').click(function(e) {
-        e.stopPropagation(); 
-    });
-
-    $("body").click(function(){
-        $('.search-icon').find('i').addClass('fa-search').removeClass('fa-times');
-        $('.search-line').slideUp("500");
-    });
-
-    $(window).scroll(function() {    
-        var about = $('#about').offset().top + 100;
-        var services = $('#services').offset().top + 100;
-        var news = $('#news').offset().top + 100;
-        var footerTop = $('footer .section-top').offset().top + 100;
-        var footerBottom = $('footer .section-bottom').offset().top + 100;
-
-        if (about - ($(window).scrollTop()+$(window).height()) < 0) {
-            $('#about .articleSec').addClass('fadeInLeft');
-            $('#about .hexagon-img').addClass('zoomIn');
-        } 
-
-        if (services - ($(window).scrollTop()+$(window).height()) < 0) {
-            $('#services .box-wrap').addClass('fadeInRight');
-        } 
-
-        if (news - ($(window).scrollTop()+$(window).height()) < 0) {
-            $('#news .item').addClass('zoomIn');
-        } 
-
-        if (footerTop - ($(window).scrollTop()+$(window).height()) < 0) {
-            $('footer .section-top h4').addClass('fadeInRight');
-            $('footer .section-top .socialM').addClass('fadeInRight');
-            $('footer .section-top .socialName').addClass('fadeInRight');
-        } 
-
-        if (footerBottom - ($(window).scrollTop()+$(window).height()) < 0) {
-            $('footer .section-bottom .navbar-brand').addClass('zoomIn');
-            $('footer .section-bottom .h6').addClass('fadeIn');
-            $('footer .section-bottom h6').addClass('fadeIn');
-            $('footer .section-bottom li').addClass('fadeIn');
-        } 
-    }); 
-
-    // $('#services .box').each(function(){
-    //     var truncated = $(this).find('p').text();
-    //     truncated = $.trim(truncated).substring(0,68) + '...';
-    //     $(this).find('p').text(truncated);
-    // });
-
-    $('#whatIs .video-wrap').click(function() {
-        $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/-uQz524deas?rel=0&enablejsapi=1&autoplay=1" frameborder="0"></iframe>');
-        $(this).addClass('none-overlay');
+        responsive : {
+            0: {
+               items: 1
+            },
+            500: {
+               items: 2
+            },
+            768 : {
+                items: 3
+            },
+            991 : {
+                items: 4
+            },
+            1200 : {
+                items: 5
+            }
+        }
     });
 });
-
-
-
