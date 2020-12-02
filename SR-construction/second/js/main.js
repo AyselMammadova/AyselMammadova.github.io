4cf092c1d999ea84ea03003abd00aec9f2694d3a
Aysel <a1@1-iMac.local>
2020-12-02 17:30:45 +0400

    second start

---
+++ SR-construction/second/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // slider
+    $('.slider').owlCarousel({ 
+        loop: true,
+        lazyLoad:true,
+        mouseDrag: false,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        autoplaySpeed: 700,
+        dots: false,
+        nav: false,
+        items: 1,
+        animateOut: 'fadeOut'
+    });
+});
