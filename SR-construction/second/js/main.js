259010e6a57347ffa2aacb8df14f2fd39bd820a4
Aysel Mammadova <ayselmemmedova9317@gmail.com>
2020-12-06 22:17:31 +0400

    project second

--- SR-construction/second/js/main.js
+++ SR-construction/second/js/main.js
@@ -12,4 +12,29 @@ $(document).ready(function() {
         items: 1,
         animateOut: 'fadeOut'
     });
+
+    // hamburger menu
+    $('body').click(function(e) {
+        $('.menu-icon').removeClass('close', 700);
+        $('.right-side-wrapper').removeClass('change-border', 800);
+        $('.menu-inside-pages').slideUp(700);
+        $('.navbar-nav').animate({right:'-100%'}, 200);
+    });
+    $('.menu-icon').click(function(e) {
+        e.stopPropagation();
+        if(!($(this).hasClass('close'))) {
+            $(this).addClass('close', 700);
+            $('.right-side-wrapper').addClass('change-border', 700);
+            $('.menu-inside-pages').slideDown(700);
+            $('.navbar-nav').animate({right:'4.94792%'}, 1000);
+        } else {
+            $(this).removeClass('close', 700);
+            $('.right-side-wrapper').removeClass('change-border', 800);
+            $('.menu-inside-pages').slideUp(700);
+            $('.navbar-nav').animate({right:'-100%'}, 200);
+        }
+    });
+    $('.menu-inside-pages').click(function(e) {
+        e.stopPropagation();
+    });
 });
