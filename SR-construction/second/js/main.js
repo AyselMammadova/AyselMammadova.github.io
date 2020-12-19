90816c0ff0a17f7f8812d298e7ee9a766b0ef3e0
Aysel Mammadova <ayselmemmedova9317@gmail.com>
2020-12-19 17:51:21 +0400

    tooltip

--- SR-construction/second/js/main.js
+++ SR-construction/second/js/main.js
@@ -40,4 +40,42 @@ $(document).ready(function() {
     //         $(this).parents().eq(1).find('.floor').addClass('hover-rect');
     //     }
     // });
+
+    // var myPolygon = document.getElementById("myPolygon");
+    // var myTooltip = document.getElementById("myTooltip");
+
+    // myPolygon.addEventListener("mouseover", showPopup);
+    // myPolygon.addEventListener("mouseout", hidePopup);
+
+    // function showPopup(evt) {
+    //     var iconPos = myPolygon.getBoundingClientRect();
+    //     myTooltip.style.left = (iconPos.right - 200) + "px";
+    //     myTooltip.style.top = (window.scrollY + iconPos.top - 10) + "px";
+    //     myTooltip.style.display = "block";
+    //     myPolygon.classList.add("active-tooltip");
+    // }
+
+    // function hidePopup(evt) {
+    //     myTooltip.style.display = "none";
+    //     myPolygon.classList.remove("active-tooltip");
+    // }
+
+    var myPolygon = $('.floor');
+    var myTooltip = $('#myTooltip');
+
+    myPolygon.mouseover(function(e) {
+        var floorN = $(this).parent().data('floor');
+        var buildingN = $(this).parent().data('building');
+        myTooltip.find('.number-b span').text(buildingN);
+        myTooltip.find('.number-f span').text(floorN);
+        myTooltip.show();
+        var polygonPos = $(this).offset();
+        myTooltip.css('left', (e.pageX - 100) + 'px');
+        myTooltip.css('top', ($(window).scrollTop() + polygonPos.top - 40) + 'px');
+        console.log(polygonPos.left);
+    });
+
+    myPolygon.mouseout(function(e) {
+        myTooltip.hide();
+    });
 });
