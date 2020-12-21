0aa5bbe2e021544e2de2565dcad31f5e9282f5b0
Aysel <a1@1-iMac.local>
2020-12-21 16:22:13 +0400

    tooltip last

--- SR-construction/first/js/main.js
+++ SR-construction/first/js/main.js
@@ -16,21 +16,16 @@ $(document).ready(function() {
     var myPolygon = $('.floor');
     var myTooltip = $('#myTooltip');
 
-    myPolygon.mouseover(function(e) {
+    myPolygon.mousemove(function(e) {
         var floorN = $(this).parent().data('floor');
         var buildingN = $(this).parent().data('building');
-        myTooltip.find('.number-b span').text(buildingN);
-        myTooltip.find('.number-f span').text(floorN);
-        myTooltip.show();
-        var polygonPos = $(this).offset();
-        
-        myTooltip.css('transform', 'translate('+ e.pageX + 'px' + ',' + e.pageY + 'px' +')');
-        // myTooltip.css('left', (e.pageX - 136) + 'px');
-        // myTooltip.css('top', e.pageY + 'px');
-        console.log(polygonPos.left);
+        $('.tooltip-box-wrapper').html('');
+        $('.tooltip-box-wrapper').append('<div class="tooltip-box"><div class="number-b"><span class="title">Bina</span><span class="number">' + buildingN + '</span></div><div class="number-f"><span class="title">Mərtəbə</span><span class="number">' + floorN + '</span></div></div>');
+        myTooltip.css('transform', 'translate('+ e.clientX + 'px' + ',' + e.clientY + 'px' +')');
+        myTooltip.css('opacity', '1');
     });
 
     myPolygon.mouseout(function(e) {
-        myTooltip.hide();
+        myTooltip.css('opacity', '0');
     });
 });
