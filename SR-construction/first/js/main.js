5ef25d4463fc30b518c80cafad68803e91165403
Aysel <a1@1-iMac.local>
2020-12-21 12:53:25 +0400

    diger duzelishler

--- SR-construction/first/js/main.js
+++ SR-construction/first/js/main.js
@@ -12,4 +12,25 @@ $(document).ready(function() {
         items: 1,
         animateOut: 'fadeOut'
     });
+
+    var myPolygon = $('.floor');
+    var myTooltip = $('#myTooltip');
+
+    myPolygon.mouseover(function(e) {
+        var floorN = $(this).parent().data('floor');
+        var buildingN = $(this).parent().data('building');
+        myTooltip.find('.number-b span').text(buildingN);
+        myTooltip.find('.number-f span').text(floorN);
+        myTooltip.show();
+        var polygonPos = $(this).offset();
+        
+        myTooltip.css('transform', 'translate('+ e.pageX + 'px' + ',' + e.pageY + 'px' +')');
+        // myTooltip.css('left', (e.pageX - 136) + 'px');
+        // myTooltip.css('top', e.pageY + 'px');
+        console.log(polygonPos.left);
+    });
+
+    myPolygon.mouseout(function(e) {
+        myTooltip.hide();
+    });
 });
