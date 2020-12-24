ea2d9fa24392a9a66912c7cd33ba9341be375f1a
Aysel <a1@1-iMac.local>
2020-12-24 13:08:34 +0400

    responsive floor

--- SR-construction/first/js/main.js
+++ SR-construction/first/js/main.js
@@ -13,8 +13,26 @@ $(document).ready(function() {
         animateOut: 'fadeOut'
     });
 
+
+    // tooltip svg
+
     var myPolygon = $('.floor');
     var myTooltip = $('#myTooltip');
+    // var w = $(window);
+
+    // function checkWidth() {
+    //     if (w.width() < 990.91) {
+    //         $('#myTooltip').addClass('mob-position');
+    //     } else {
+    //         $('#myTooltip').removeClass('mob-position');
+    //     }
+    // }
+    
+    // window.onload = function() {
+    //   checkWidth();
+    // };
+    
+    // w.resize(checkWidth);
 
     myPolygon.mousemove(function(e) {
         var floorN = $(this).parent().data('floor');
@@ -22,12 +40,17 @@ $(document).ready(function() {
         $('.tooltip-box-wrapper').html('');
         $('.tooltip-box-wrapper').append('<div class="tooltip-box"><div class="number-b"><span class="title">Bina</span><span class="number">' + buildingN + '</span></div><div class="number-f"><span class="title">Mərtəbə</span><span class="number">' + floorN + '</span></div></div>');
         myTooltip.css('transform', 'translate('+ e.clientX + 'px' + ',' + e.clientY + 'px' +')');
+        // $('.mob-position').css('transform', 'translate('+ e.clientX + 'px' + ',' + e.clientY + 'px' +')');
         myTooltip.css('opacity', '1');
+        // console.log(w.scrollLeft());
     });
 
     myPolygon.mouseout(function(e) {
         myTooltip.css('opacity', '0');
     });
+
+
+    // popup notification for scroll mob
 
     setTimeout(function() {
         $('.mob-scroll-notification').addClass('hide');
@@ -40,17 +63,20 @@ $(document).ready(function() {
         $('.social-media li').removeClass('black', 300);
     });
 
-     // hamburger menu
+
+    // hamburger menu
     $('.menu-icon').click(function(e) {
         e.stopPropagation();
         if(!($(this).hasClass('close'))) {
             $(this).addClass('close', 300);
             $('#menu').slideDown(300);
             $('.social-media li').addClass('black', 500);
+            $('header nav .call-us').addClass('show', 500);
         } else {
             $(this).removeClass('close', 300);
             $('#menu').slideUp(300);
             $('.social-media li').removeClass('black', 300);
+            $('header nav .call-us').removeClass('show', 300);
         }
     });
     $('#menu').click(function(e) {
